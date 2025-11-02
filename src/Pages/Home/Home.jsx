17fb49eeed40f3e70771dae5348c7bd89980baf3
Abhijit <abhijitthander38@gmail.com>
2025-11-02 21:21:03 +0530

    feed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import Feed from "../../Components/Feed/Feed";
 
-const Home = ({sidebar}) => {
+const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "container-full"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
