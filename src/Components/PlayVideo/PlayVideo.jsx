09ef29de0140213e5456fa0c7aabf87004925a0c
Abhijit <abhijitthander38@gmail.com>
2025-11-04 13:14:13 +0530

    complete

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 import "./PlayVideo.css";
-import Video1 from "../../assets/video.mp4";
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
+import { Api_Key, value_converter } from "../../Data";
+import { useParams } from "react-router-dom";
 
 const PlayVideo = () => {
+  const { videoId } = useParams();
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentsData, setCommentsData] = useState([]);
+
+  const fetchVideoData = async () => {
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${Api_Key}`;
+    await fetch(videoDetails_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  const fetchChannelData = async () => {
+    if (apiData) {
+      const channelDetails_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${Api_Key}`;
+      await fetch(channelDetails_url)
+        .then((res) => res.json())
+        .then((data) => setChannelData(data.items[0]));
+    }
+    //fetch comments data
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${Api_Key}`;
+    await fetch(comment_url)
+      .then((res) => res.json())
+      .then((data) => setCommentsData(data.items));
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, [videoId]);
+
+  useEffect(() => {
+    fetchChannelData();
+  }, [apiData]);
+
   return (
     <div className="play-video-container">
-      <video src={Video1} controls autoPlay muted></video>
-      <h3>Best Youtube Channel to learn web development</h3>
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
       <div className="play-video-info">
-        <p>154k views &bull; 2 days ago</p>
+        <p>
+          {apiData ? value_converter(apiData.statistics.viewCount) : "16K"}{" "}
+          Views &bull;{" "}
+          {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}
+        </p>
         <div>
           <span>
             <img src={like} alt="" />
-            123K
+            {apiData ? value_converter(apiData.statistics.likeCount) : ""}
           </span>
           <span>
-            <img src={dislike} alt="" />2
+            <img src={dislike} alt="" />
           </span>
           <span>
             <img src={share} alt="" />
@@ -35,93 +81,57 @@ const PlayVideo = () => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img
+          src={channelData ? channelData.snippet.thumbnails.default.url : ""}
+          alt=""
+        />
         <div>
-          <p>Greatstack</p>
-          <span>10M subscribers</span>
+          <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+          <span>
+            {channelData
+              ? value_converter(channelData.statistics.subscriberCount)
+              : ""}{" "}
+            Subscriber
+          </span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="description">
-        <p>Channel thats make learning easy</p>
-        <p>Subscribe for more videos like this.</p>
+        <p>
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description here"}
+        </p>
         <hr />
-        <h4>130 comments</h4>
-        <div className="add-comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Thander <span>1Days ago</span>
-            </h3>
-            <p>A great channel to learn web development</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>144</span>
-              <img src={dislike} alt="" />
-              <span>2</span>
+        <h4>{apiData ? apiData.statistics.commentCount : 0} Comments</h4>
+
+        {commentsData.map((item, idx) => {
+          return (
+            <div key={idx} className="add-comment">
+              <img
+                src={item.snippet.topLevelComment.snippet.authorProfileImageUrl}
+                alt=""
+              />
+              <div>
+                <h3>
+                  {item.snippet.topLevelComment.snippet.authorDisplayName}{" "}
+                  <span>1Days ago</span>
+                </h3>
+                <p className="comment-text">
+                  {item.snippet.topLevelComment.snippet.textOriginal}
+                </p>
+                <div className="comment-action">
+                  <img src={like} alt="" />
+                  <span>{item.snippet.topLevelComment.snippet.likeCount}</span>
+                  <img src={dislike} alt="" />
+                  <span>
+                    {item.snippet.topLevelComment.snippet.dislikeCount}
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="add-comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Thander <span>1Days ago</span>
-            </h3>
-            <p>A great channel to learn web development</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>144</span>
-              <img src={dislike} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-        </div>
-        <div className="add-comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Thander <span>1Days ago</span>
-            </h3>
-            <p>A great channel to learn web development</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>144</span>
-              <img src={dislike} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-        </div>
-        <div className="add-comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Thander <span>1Days ago</span>
-            </h3>
-            <p>A great channel to learn web development</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>144</span>
-              <img src={dislike} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-        </div>
-        <div className="add-comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Thander <span>1Days ago</span>
-            </h3>
-            <p>A great channel to learn web development</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>144</span>
-              <img src={dislike} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Components/Recomended/Recomended.jsx
+++ src/Components/Recomended/Recomended.jsx
@@ -1,73 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Recomended.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
+import { Api_Key, value_converter } from "../../Data.js";
+import { Link } from "react-router-dom";
 
-const Recomended = () => {
+const Recomended = ({ categoryId }) => {
+  const [apiData, setApiData] = useState([]);
+  const fetchRecomendedVideos = async () => {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=IN&videoCategoryId=${categoryId}&key=${Api_Key}`
+    );
+    const data = await response.json();
+    setApiData(data.items);
+  };
+
+  useEffect(() => {
+    fetchRecomendedVideos();
+  }, [categoryId]);
+
   return (
     <div className="recomended">
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail2} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail3} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail4} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail5} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail6} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail7} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that helps you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
+      {apiData.map((item, idx) => {
+        return (
+          <Link
+            to={`/watch/${item.snippet.categoryId}/${item.id}`}
+            key={idx}
+            className="side-video-list"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <div className="vid-info">
+              <h4 className="title">{item.snippet.title} </h4>
+              <p className="channel-name">{item.snippet.channelTitle}</p>
+              <p className="views">
+                {value_converter(item.statistics.viewCount)} Views
+              </p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
